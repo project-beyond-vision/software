0fdad3c2cbef55209af48067b14d329eeef2cac4
LAPTOP-K3LA6L88\wamik <wamikamalik@hotmail.com>
2022-10-18 14:26:54 +0800

    Add predictor function

---
+++ predictor.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import keras
+
+
+
+activity = ['Fall', 'Jogging', 'Sitting action', 'Walking', 'climbing stairs', 'getting up']
+
+mu = [0.012236175547560521, -0.844028325626327, -0.1543912455955434, 0.2556826382272823, 1.5261069734771806, -0.2920010667935128]
+sigma = [0.34769250871819196, 0.5536567307930977, 0.42467598155613845, 35.02454787273533, 35.0604674029397, 27.524737149885194]
+
+def normalize_features(data, mu_data, sigma_data):
+    return (data - mu_data)/sigma_data
+
+def predictor(inputs):
+
+    Test_data = inputs
+
+    for i in range(0,6):
+        for j in range(0,60):
+            Test_data[j][i] = normalize_features(Test_data[j][i], mu[i], sigma[i])
+
+    # print("test data shape: ", Test_data.shape)
+
+    model = keras.models.load_model('best_model_lstm.h5')
+
+    Test_data = Test_data.astype("float32")
+
+    pred = model.predict(Test_data)
+    prediction = np.argmax(pred, axis=1)
+
+    return activity[prediction]
