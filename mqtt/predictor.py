88df18c48e2c141c9e4ae69764a0607cb5bd761a
LAPTOP-K3LA6L88\wamik <wamikamalik@hotmail.com>
2022-10-19 13:47:21 +0800

    Fix predictor array shape

--- mqtt/predictor.py
+++ mqtt/predictor.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import keras
 
-
+STEP_SIZE = 60
+NUM_FEATURES = 6
 
 activity = ['Fall', 'Jogging', 'Sitting action', 'Walking', 'climbing stairs', 'getting up']
 
@@ -16,17 +17,22 @@ def predictor(inputs):
 
     Test_data = inputs
 
-    for i in range(0,6):
-        for j in range(0,60):
+    for i in range(0,NUM_FEATURES):
+        for j in range(0,STEP_SIZE):
             Test_data[j][i] = normalize_features(Test_data[j][i], mu[i], sigma[i])
+
+    Test_data = np.asarray(Test_data).reshape(1, STEP_SIZE, NUM_FEATURES)
 
     # print("test data shape: ", Test_data.shape)
 
-    model = keras.models.load_model('best_model_lstm.h5')
+    # Use absolute path to the .h5 file
+    model = keras.models.load_model('best_model_lsnet.h5')
 
     Test_data = Test_data.astype("float32")
 
     pred = model.predict(Test_data)
-    prediction = np.argmax(pred, axis=1)
+    prediction = np.argmax(pred, axis=1)[0]
+
+    # print(prediction.shape)
 
     return activity[prediction]
