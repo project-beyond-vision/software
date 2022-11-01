9c56541ac06cb1f8b079206ef8ea7a8933f319fe
CFZeon <ennzerothreetwo@gmail.com>
2022-11-01 17:36:04 +0800

    update laptop code for scalability

--- mqtt/beltimutest.py
+++ mqtt/beltimutest.py
@@ -16,16 +16,16 @@ def main():
     temp = time.perf_counter()
     tempthreshold = time.perf_counter()
     while True:
-        if time.perf_counter() - temp > 0.05:
-            temp = time.perf_counter()
-            data = {"x" : time.perf_counter(), "y" : time.perf_counter()/2, "z" : time.perf_counter()/3, "rx" : time.perf_counter(), "ry" : time.perf_counter()/2, "rz" : time.perf_counter()/3 }
-            data = json.dumps(data)
-            client.publish("group_05/imu/data", data)
+        # if time.perf_counter() - temp > 0.05:
+        #     temp = time.perf_counter()
+        #     data = {"x" : time.perf_counter(), "y" : time.perf_counter()/2, "z" : time.perf_counter()/3, "rx" : time.perf_counter(), "ry" : time.perf_counter()/2, "rz" : time.perf_counter()/3 }
+        #     data = json.dumps(data)
+        #     client.publish("group_05/imu/data", data)
         
-        if time.perf_counter() - tempthreshold > 12:
-            print("threshold")
-            tempthreshold = time.perf_counter()
-            client.publish("group_05/imu/threshold", "yes")
+        # if time.perf_counter() - tempthreshold > 12:
+        #     print("threshold")
+        #     tempthreshold = time.perf_counter()
+        #     client.publish("group_05/imu/threshold", "yes")
         time.sleep(0.001)
 
 def setup(hostname: str) -> mqtt.Client:
@@ -50,7 +50,7 @@ def on_message(client, userdata, msg):
     print("send gps")
     # Payload is in msg. We convert it back to a Python dictionary.
     # recv_dict = json.loads(msg.payload)
-    data = {"lat": 1.3, "long": 2.71}
+    data = {"id": 1,"lat": 1.3, "long": 2.71}
     client.publish("group_05/gps", json.dumps(data))
     # send_message_to_user()
 
--- mqtt/laptop.py
+++ mqtt/laptop.py
@@ -19,16 +19,9 @@ class MqttManager():
 
     def __init__(self, client):
         self.client = client
+        self.datastore = {}
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        self.belt_imu_queue = []
-        self.predqueue = []
-        self.stick_threshold = False
-        self.stick_threshold_time = time.perf_counter()
-        self.location = [] # for lat and long
-        self.gps_reason = ""
-
-        self.predqueuebuffer = [] # to store prediction queue on fall prediction and ensure it is not overwritten
 
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code", str(rc))
@@ -40,103 +33,111 @@ class MqttManager():
 
     def on_message(self, client, userdata, msg):
         # print(msg.topic, msg.payload.decode("utf-8"))
-        if (msg.topic == "group_05/imu/data"):
-            self.store_imu_data(msg.payload.decode("utf-8"))
-        elif (msg.topic == "group_05/imu/threshold"):
-            self.update_threshold()
-        elif (msg.topic == "group_05/gps"):
-            self.update_location(msg.payload.decode("utf-8"))
-        elif (msg.topic == "group_05/flame"):
-            self.handle_flame()
-        elif (msg.topic == "group_05/panic"):
-            self.handle_panic()
+        inputmsg = msg.payload.decode("utf-8")
 
-    def store_database_entry(self):
+        data = json.loads(inputmsg)
+
+        if not data["id"] in self.datastore:
+            self.datastore[data["id"]] = {}
+            self.datastore[data["id"]]["imu_queue"] = []
+            self.datastore[data["id"]]["gps_reason"] = ""
+            self.datastore[data["id"]]["chat_id"] = data["chat_id"]
+            self.datastore[data["id"]]["location"] = []
+            self.datastore[data["id"]]["predqueue"] = []
+            self.datastore[data["id"]]["predqueuebuffer"] = []
+
+        if (msg.topic == "group_05/imu/data"):
+            self.store_imu_data(data)
+        elif (msg.topic == "group_05/gps"):
+            self.update_location(data)
+        elif (msg.topic == "group_05/flame"):
+            self.handle_flame(data["id"])
+        elif (msg.topic == "group_05/panic"):
+            self.handle_panic(data["id"])
+
+    def store_database_entry(self, id):
         now = datetime.now()
 
         #TODO: handle case where predqueuebuffer less than 4
-        while len(self.predqueuebuffer) < PREDQUEUE:
+        while len(self.datastore[id]["predqueuebuffer"]) < PREDQUEUE:
             # append NO PREDICTION to the START of the queue
-            self.predqueuebuffer.insert(0, "") 
+            self.datastore[id]["predqueuebuffer"].insert(0, "") 
         
-        obj = {"time":now.isoformat(), "lat":self.location[0], "long":self.location[1], 
-        "isflame": self.gps_reason == FLAME_MESSAGE,
-        "ispanic": self.gps_reason == PANIC_MESSAGE,
-        "pred1": self.predqueuebuffer[0], 
-        "pred2": self.predqueuebuffer[1], 
-        "pred3": self.predqueuebuffer[2], 
-        "pred4": self.predqueuebuffer[3],
-        "pred5": self.predqueuebuffer[4], 
-        "pred6": self.predqueuebuffer[5], 
-        "pred7": self.predqueuebuffer[6], 
-        "pred8": self.predqueuebuffer[7], 
-        "pred9": self.predqueuebuffer[8], 
-        "pred10": self.predqueuebuffer[9]}
+        obj = {"time":now.isoformat(), "lat":self.datastore[id]["location"][0], "long":self.datastore[id]["location"][1], 
+        "is_flame": self.datastore[id]["gps_reason"] == FLAME_MESSAGE,
+        "is_panic": self.datastore[id]["gps_reason"] == PANIC_MESSAGE,
+        "pred1": self.datastore[id]["predqueuebuffer"][0], 
+        "pred2": self.datastore[id]["predqueuebuffer"][1], 
+        "pred3": self.datastore[id]["predqueuebuffer"][2], 
+        "pred4": self.datastore[id]["predqueuebuffer"][3],
+        "pred5": self.datastore[id]["predqueuebuffer"][4], 
+        "pred6": self.datastore[id]["predqueuebuffer"][5], 
+        "pred7": self.datastore[id]["predqueuebuffer"][6], 
+        "pred8": self.datastore[id]["predqueuebuffer"][7], 
+        "pred9": self.datastore[id]["predqueuebuffer"][8], 
+        "pred10": self.datastore[id]["predqueuebuffer"][9]}
 
         x = requests.post(db_url, json=obj)
 
     def store_imu_data(self, data):
-        data = json.loads(data)
         imu_entry = [data["x"], data["y"], data["z"], data["rx"], data["ry"], data["rz"]]
-        while len(self.belt_imu_queue) >= BELT_IMU_QUEUESIZE:
-            self.belt_imu_queue.pop(0)
-        self.belt_imu_queue.append(imu_entry)
+        while len(self.datastore[data["id"]]["imu_queue"]) >= BELT_IMU_QUEUESIZE:
+            self.datastore[data["id"]]["imu_queue"].pop(0)
+        self.datastore[data["id"]]["imu_queue"].append(imu_entry)
     
     def send_buzzer_trigger(self): 
         self.client.publish("group_05/buzzer", "TEST SENDING BUZZER TRIGGER")
     
     def update_location(self, data):
-        data = json.loads(data)
         # technically update location should be the last step in the api call after fall confirmed
-        self.location = [data["lat"], data["long"]]
-        msg = f'{self.gps_reason} at latitide: {data["lat"]} longitude: {data["long"]}'
-        send_telegram_message(msg)
-        self.gps_reason = EMPTY_STRING
+        self.datastore[data["id"]]["location"] = [data["lat"], data["long"]]
+        msg = f'{self.datastore[data["id"]]["gps_reason"]} at latitide: {data["lat"]} longitude: {data["long"]}'
+        send_telegram_message(msg, self.datastore[data["id"]]["chat_id"])
+        self.datastore[data["id"]]["gps_reason"] = EMPTY_STRING
         print("telegram message sent")
 
         # UNCOMMENT THE LINES BELOW ONLY IF THE SERVER IS RUNNING
-        self.store_database_entry()
-        print("post request sent")
-    
-    def update_threshold(self):
-        self.stick_threshold = True
-        self.stick_threshold_time = time.perf_counter()
+        try:
+            self.store_database_entry(data["id"])
+            print("post request sent")
+        except Exception as e:
+            print(f"caught {e}")
+            print(f"Is the database server running and connected to your local network?")
 
-    def handle_flame(self):
-        self.gps_reason = FLAME_MESSAGE
+    def handle_flame(self, id):
+        self.datastore[id]["gps_reason"] = FLAME_MESSAGE
         self.client.publish("group_05/gps_signal", "GPS trigger message")
         
-    def handle_panic(self):
+    def handle_panic(self, id):
         # if this function is called, send gps data immediately to user
-        self.gps_reason = PANIC_MESSAGE
+        self.datastore[id]["gps_reason"] = PANIC_MESSAGE
         self.client.publish("group_05/gps_signal", "GPS trigger message")
 
-    def make_prediction(self):        
+    def make_prediction(self, id):        
         # DO NOT make prediction if there is less than 60 imu entries
-        if len(self.belt_imu_queue) < BELT_IMU_QUEUESIZE:
+        if len(self.datastore[id]["imu_queue"]) < BELT_IMU_QUEUESIZE:
             return
-        while len(self.belt_imu_queue) > BELT_IMU_QUEUESIZE:
-            self.belt_imu_queue.pop(0)
-        data = np.array(self.belt_imu_queue)
+        while len(self.datastore[id]["imu_queue"]) > BELT_IMU_QUEUESIZE:
+            self.datastore[id]["imu_queue"].pop(0)
+        data = np.array(self.datastore[id]["imu_queue"])
         pred = predictor(data) # dummy variable until api call is done
         # pred = "Fall" #test line remember to comment
         print(pred)
         # update predqueue
-        while len(self.predqueue) >= PREDQUEUE:
-            self.predqueue.pop(0)
-        self.predqueue.append(pred)
+        while len(self.datastore[id]["predqueue"]) >= PREDQUEUE:
+            self.datastore[id]["predqueue"].pop(0)
+        self.datastore[id]["predqueue"].append(pred)
 
-        # 1 is dummy number for when fall detected
-        if pred == "Fall" and self.stick_threshold:
+        if pred == "Fall":# and self.stick_threshold:
             print("GPS Trigger")
-            self.gps_reason = "Fall detected"
-            self.predqueuebuffer = self.predqueue
+            self.datastore[id]["gps_reason"] = "Fall detected"
+            self.datastore[id]["predqueuebuffer"] = self.datastore[id]["predqueue"]
             # fall confirmed, send trigger for gps data
             self.client.publish("group_05/gps_signal", "GPS trigger message")
     
 
-def send_telegram_message(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}"
+def send_telegram_message(msg, chatid):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chatid}&text={msg}"
     print(requests.get(url).json()) # this sends the message and prints out the return value
 
 
@@ -151,14 +152,10 @@ def main():
     predtimer = time.perf_counter()
     while True:
         # make predictions every PRED_INTERVAL seconds
-        if time.perf_counter() - predtimer > PRED_INTERVAL:
-            predtimer = time.perf_counter()
-            manager.make_prediction()
-
-        # check if stick threshold is true
-        if manager.stick_threshold:
-            if time.perf_counter() - manager.stick_threshold_time > STICK_THRESHOLD_TIME:
-                manager.stick_threshold = False
+        for key in manager.datastore:
+            if time.perf_counter() - predtimer > PRED_INTERVAL:
+                predtimer = time.perf_counter()
+                manager.make_prediction(key)
         # suspend the thread to make my cpu usage not 100%
         time.sleep(0.001)
 
